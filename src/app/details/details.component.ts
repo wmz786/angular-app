c8a4f7c44a463fa7e4626fa6d7e22ec17e822500
wmz786 <waismalikzada9@gmail.com>
2023-12-27 09:56:37 +0430

    get route parameter

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      details works!
-    </p>
-  `,
-  styleUrl: './details.component.css'
+  template: ` <p>details works! {{ housingLocationId }}</p> `,
+  styleUrl: './details.component.css',
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
